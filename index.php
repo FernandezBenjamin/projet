04e0d2e3cb554d7b7f873bbb33032f9343b10623
Zitox <fernandez.benjamin@outlook.com>
2017-07-11 20:01:40 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,4 @@ require_once "tools/functions.php";
 include "header.php";
 
 
-echo "<br><button class='test_button' onclick=\"refresh_popular_movies_db('$client_id','1')\">test</button><br>";
-
-
 include "footer.php";
